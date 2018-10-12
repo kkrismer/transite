a67a3f46940b65291eaf8edc7fd09492e144be3b
Konstantin Krismer <krismer@mit.edu>
2018-10-12 15:45:05 -0400

    Improved documentation of data objects

--- R/data.R
+++ R/data.R
@@ -1,4 +1,13 @@
-#' Transite motifs
+#' Transite Motif Database
+#'
+#' The Transite motif database contains sequence motifs and associated
+#' \emph{k}-mers of more than 100 different RNA-binding proteins, obtained from
+#' publicly available motif databases.
+#' @references
+#' \url{http://cisbp-rna.ccbr.utoronto.ca/}
+#'
+#' \url{http://rbpdb.ccbr.utoronto.ca/}
+#'
 #'
 #' @format A list of lists with the following components:
 #' \tabular{rl}{
@@ -17,8 +26,15 @@
 #' }
 "motifs"
 
-#' A toy data set with gene expression measurements and 3'-UTR sequences
-#' of 1000 genes
+#' Toy Gene Expression Data Set
+#'
+#' This object contains a toy data set based on gene expression measurements
+#' and 3'-UTR sequences of 1000 genes. It comprises three data frames with
+#' RefSeq identifiers, log fold change values, and 3'-UTR sequences of genes,
+#' which are either upregulated or downregulated after some hypothetical
+#' treatment, as well as all measured genes. The actual values are not
+#' important. This data set merely serves as an example input for various
+#' functions.
 #'
 #' @format A list with the following components:
 #' \tabular{rl}{
@@ -31,7 +47,9 @@
 "ge"
 
 
-#' A toy motif matrix.
+#' Toy Motif Matrix
+#'
+#' This toy motif matrix is used in code examples for various functions.
 #'
 #' @format A data frame with four columns (A, C, G, U) and seven
 #' rows (position 1 - 7)
